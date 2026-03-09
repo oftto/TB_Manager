f2625e77e7055f84302be33d554b4c181c518b78
Yeonjoo <syeonjoo@gmail.com>
2026-03-09 21:08:10 +0900

    Update VIX symbol in get_stooq function

--- market_signal.py
+++ market_signal.py
@@ -72,7 +72,7 @@ change = (close.iloc[-1] - close.iloc[-2]) / close.iloc[-2] * 100
 # -------------------------
 # VIX
 # -------------------------
-vix = get_stooq("vix")
+vix = get_stooq("^VIX")
 
 if vix.empty:
     send("🚨 ERROR: VIX data failed")
