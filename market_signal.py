ba46c12a15417d0309a2f2ccee71d44937975f76
Yeonjoo <syeonjoo@gmail.com>
2026-03-09 20:34:34 +0900

    Update QQQ data retrieval method

--- market_signal.py
+++ market_signal.py
@@ -15,11 +15,8 @@ def send(msg):
 
 # QQQ data (retry)
 for i in range(3):
-    #qqq = yf.Ticker("QQQ").history(period="1mo")
-    qqq = yf.download("QQQ",
-                 period = "6mo",
-                 progress = False
-                )
+    qqq = yf.Ticker("QQQ")
+    qqq = qqq.history(period="1mo")
     if not qqq.empty:
         break
 
