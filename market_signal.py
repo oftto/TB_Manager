a7fd68139bc8bf286446c1b952d7f961e136a829
Yeonjoo <syeonjoo@gmail.com>
2026-03-09 20:21:38 +0900

    Change data source from QQQ to PEP for download

--- market_signal.py
+++ market_signal.py
@@ -15,7 +15,11 @@ def send(msg):
 
 # QQQ data (retry)
 for i in range(3):
-    qqq = yf.Ticker("QQQ").history(period="1mo")
+    #qqq = yf.Ticker("QQQ").history(period="1mo")
+    qqq = yf.download("PEP",
+                 period = "6mo",
+                 progress = False
+                )
     if not qqq.empty:
         break
 
