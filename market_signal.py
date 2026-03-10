cbc859be117975980a3e629bf8d8278145236cbc
Yeonjoo <syeonjoo@gmail.com>
2026-03-10 22:46:41 +0900

    Update market_signal.py

--- market_signal.py
+++ market_signal.py
@@ -10,7 +10,7 @@ CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 # total stocks
 # -------------------------
 url = os.environ["TARGET_SHEET"]
-df = pd.read.csv(url)
+df = pd.read_csv(url)
 holding=df.iloc[0,-1]
 
 
